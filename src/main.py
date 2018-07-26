ce1f8da8bc5115ec0d8b066dcb0f79f5af24f84c
Philippe <nphilougmail.com>
2018-07-26 19:08:58 +0200

    triplets from embedding

--- src/main.py
+++ src/main.py
@@ -71,34 +71,34 @@ def get_vgg(input_shape, output_shape):
     return model
 
 
-def generate_hard_triplets(x_train, y_train, count=100):
+def generate_hard_triplets(dist, data, y, count=100):
     i = 0
     dlist = []
     clist = []
 
-    x_train_2d = x_train.reshape(x_train.shape[0], x_train.shape[1] * x_train.shape[2])
+    # x_train_2d = x_train.reshape(x_train.shape[0], x_train.shape[1] * x_train.shape[2])
     # dist_pos = np.linalg.norm(x_train_2d - x_train_2d[:, None], axis=-1)
-    dist_pos = distance.cdist(x_train_2d, x_train_2d, metric='euclidean')
+    dist_pos = distance.cdist(dist, dist, metric='euclidean')
     np.fill_diagonal(dist_pos, np.nan)
 
     dist_neg = np.array(dist_pos, copy=True)
 
     while True:
-        anchor, cls = x_train[[i]], y_train[[i]]
+        anchor, cls = data[[i]], y[[i]]
 
         # prendre i qui maximise la distance en pos et anchor
-        pos_mask = np.array(y_train == cls)
+        pos_mask = np.array(y == cls)
         dist_pos[i][~pos_mask] = np.nan
         max_idx = np.nanargmax(dist_pos[i])
 
-        pos = x_train[[max_idx]]
+        pos = data[[max_idx]]
 
         # prendre i qui minimise la distance en pos et anchor
-        neg_mask = np.array(y_train != cls)
+        neg_mask = np.array(y != cls)
         dist_neg[i][~neg_mask] = np.nan
         min_idx = np.nanargmin(dist_neg[i])
 
-        neg = x_train[[min_idx]]
+        neg = data[[min_idx]]
 
         dlist.append([anchor, pos, neg])
         clist.append(cls)
@@ -157,25 +157,28 @@ def tensorboard():
 if __name__ == '__main__':
     path = '/Users/Philippe/Programmation/research-purposes/data/a0001-jmac_DSC1459.dng'
 
-    train_length = 1000
+    train_length = 6000
     test_length = 1000
     in_dims = (28, 28, 1)
     out_dims = 16
     LOG_DIR = '../logs'
 
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    (x_train, y), (x_test, y_test) = mnist.load_data()
     x_train = x_train.astype('float32')[::10]
     x_test = x_test.astype('float32')[::10]
-    y_train = y_train[::10]
+    y = y[::10]
     y_test = y_test[::10]
     x_train /= 255
     x_test /= 255
     print('x_train shape:', x_train.shape)
-    print(x_train.shape[0], y_train.shape, 'train samples')
+    print(x_train.shape[0], y.shape, 'train samples')
     print(x_test.shape[0], y_test.shape, 'test samples')
 
+    x_train_2d = x_train.reshape(x_train.shape[0], x_train.shape[1] * x_train.shape[2])
+    x_test_2d = x_test.reshape(x_test.shape[0], x_test.shape[1] * x_test.shape[2])
+
     #  train triplets
-    triplets_train, cls_train = generate_hard_triplets(count=train_length, x_train=x_train, y_train=y_train)
+    triplets_train, cls_train = generate_hard_triplets(count=train_length, dist=x_train_2d, data=x_train_2d, y=y)
     triplets_array = np.asarray(triplets_train)
     cls = np.asarray(cls_train)
     # train_length = cls.shape[0]
@@ -186,24 +189,13 @@ if __name__ == '__main__':
     }
 
     # test triplets (pos and neg can be useless)
-    triplets_test, cls_test = generate_hard_triplets(count=test_length, x_train=x_test, y_train=y_test)
+    triplets_test, cls_test = generate_hard_triplets(count=test_length, dist=x_test_2d, data=x_test_2d, y=y_test)
     triplets_array = np.asarray(triplets_test)
     x_triplet_test = {
         'input_1': triplets_array[:, 0].reshape(test_length, 28, 28, 1),
         'input_2': triplets_array[:, 1].reshape(test_length, 28, 28, 1),
         'input_3': triplets_array[:, 2].reshape(test_length, 28, 28, 1)
     }
-
-    for i in range(3):
-        plt.imshow(triplets_array[i, 0].reshape(28, 28), cmap='gray')
-        plt.show()
-
-        plt.imshow(triplets_array[i, 1].reshape(28, 28), cmap='gray')
-        plt.show()
-
-        plt.imshow(triplets_array[i, 2].reshape(28, 28), cmap='gray')
-        plt.show()
-
 
     #  input tensors
     anc_in = Input(shape=in_dims)
@@ -225,25 +217,27 @@ if __name__ == '__main__':
                   loss=triplet_loss)
 
     before = model.predict(x_triplet_train)
-    plt.scatter(before[:, 0], before[:, 1], c=y_train[:train_length])
+    plt.scatter(before[:, 0], before[:, 1], c=y[:train_length])
     plt.show()
 
+    print(before.shape)
+
+    triplets_train, cls_train = generate_hard_triplets(count=train_length, dist=before[:, range(out_dims)],
+                                                       data=x_train_2d, y=y)
+
+    x_triplet_train = {
+        'input_1': np.asarray(triplets_train)[:, 0].reshape(train_length, 28, 28, 1),
+        'input_2': np.asarray(triplets_train)[:, 1].reshape(train_length, 28, 28, 1),
+        'input_3': np.asarray(triplets_train)[:, 2].reshape(train_length, 28, 28, 1)
+    }
+
     # model.fit(x_triplet_train, np.ones(len(x_triplet_train['input_1'])), steps_per_epoch=256, epochs=10)
-    model.fit(x_triplet_train, cls, steps_per_epoch=32, epochs=1)
+    model.fit(x_triplet_train, cls, steps_per_epoch=128, epochs=1)
 
     x_train_encoded = model.predict(x_triplet_train)
     x_test_encoded = model.predict(x_triplet_test)
 
-    plt.scatter(x_train_encoded[:, 0], x_train_encoded[:, 1], c=y_train[:train_length])
-    plt.show()
-
-    plt.scatter(x_train_encoded[:, 1], x_train_encoded[:, 2], c=y_train[:train_length])
-    plt.show()
-
-    plt.scatter(x_train_encoded[:, 2], x_train_encoded[:, 3], c=y_train[:train_length])
-    plt.show()
-
-    plt.scatter(x_train_encoded[:, 3], x_train_encoded[:, 4], c=y_train[:train_length])
+    plt.scatter(x_train_encoded[:, 0], x_train_encoded[:, 1], c=y[:train_length])
     plt.show()
 
     # keep anchor encoding only
@@ -260,7 +254,7 @@ if __name__ == '__main__':
     #  take instead x_triplet_train['input_1']reshape(train_length,28²)
     #  knn without encoding
     neigh = KNeighborsClassifier(n_neighbors=1)
-    neigh.fit(x_train_2d, y_train)
+    neigh.fit(x_train_2d, y)
     score = neigh.score(x_test_2d, y_test)
     print(score)
 
